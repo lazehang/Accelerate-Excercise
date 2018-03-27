1f29d092e9faf7ac922ed66c54555f31757d1680
lazehang <lazehang@gmail.com>
2018-03-27 10:32:59 +0800

    day 22 asynchronous code and callbacks

---
+++ Nodejs/22-node-intro-cont/Asynchronous code and callbacks/A/app.js
@@ -0,0 +1,21 @@
+var myArray = [4, 8, 2, 7, 5];
+processArray(myArray, function(a) {
+    return a * 2;
+});
+// [ 8, 16, 4, 14, 10 ]
+
+var myArray = [7, 8, 9, 1, 2];
+processArray(myArray, function (a) {
+    return a + 5;
+});
+// [ 12, 13, 14, 6, 7 ]
+
+function processArray(array, callback) {
+	let newArray = [];
+
+	array.forEach(function(arr) {
+		newArray.push(callback(arr));
+	});
+
+	console.log(newArray);
+}
